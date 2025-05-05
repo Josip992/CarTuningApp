3fbcc64d3760473678b47f52fad0b057ea1d6ea1
Josip Barisic <workflow992@gmail.com>
2025-05-05 18:31:36 +0200

    frontend

--- car-tuning-frontend/src/components/Navbar.jsx
+++ car-tuning-frontend/src/components/Navbar.jsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
 import "../css/Navbar.css"
+import "../css/VehicleListForm.css"
 import { useAuthContext } from "../contexts/AuthContext";
 
 function Navbar(){
-    const { user, logout} = useAuthContext();
+    const { user, logout } = useAuthContext();
 
     return (
         <nav className="navbar">
-            <div className="navbar-brand">
-                <Link to="/" className="navbar-brand">Car Tuning</Link>
+            <div>
+                <Link to="/" className="navbar-brand"><img src="/images/logo/logo.jpg" alt="Car Tuning Logo" className="navbar-logo" /></Link>
             </div>
             {user ? (
                 <div>
-                <button onClick={logout}>LOGOUT</button>
+                <button 
+                onClick={logout}
+                className="button-navbar"
+                >Logout</button>
             </div>
             ):(
-            <div>
             <div className="navbar-links">
+            <div>
                 <Link to="/login" className="nav-link">Login </Link>
             </div>
-            <div className="navbar-links">
-            <Link to="/register" className="nav-link">Register </Link>
+            <div>
+                <Link to="/register" className="nav-link">Register </Link>
             </div>
             </div>    
             )
